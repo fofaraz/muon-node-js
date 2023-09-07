de8993d736c9ca896d3521a2934a4280c8638781
farazfo@yahoo.com <11iloveu005>
2023-09-07 10:20:36 +0330

    run test on local

--- test/config.js
+++ test/config.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "price_test";
-export const GATEWAY_URL = "https://testnet.muon.net";
+export const GATEWAY_URL = "http://127.0.0.1:8000";
 export const DEPLOY_CONFIG = {
-    "url": "https://testnet.muon.net/v1"
+    "url": "http://127.0.0.1:8000/v1"
 };
